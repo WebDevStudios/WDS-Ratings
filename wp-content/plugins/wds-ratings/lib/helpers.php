ebc6b7f0e798ea88f36351f0c8dad878a794b6d0
Justin Sternberg <justin@dsgnwrks.pro>
2015-02-23 16:28:44 -0500

    Clean up redundant class/function checks

--- wp-content/plugins/wds-ratings/lib/helpers.php
+++ wp-content/plugins/wds-ratings/lib/helpers.php
@@ -2,16 +2,14 @@
 
 if ( ! function_exists( 'wds_post_ratings' ) ) {
 	function wds_post_ratings( $echo = true ) {
-		if ( is_object( wds_ratings() ) && method_exists( wds_ratings(), 'fetch_ratings_template' ) ) {
-			$ratings = wds_ratings()->fetch_ratings_template();
-			
-			if ( $echo ) {
-				echo $ratings;
-			}
-			
-			return $ratings;
+		$ratings = wds_ratings()->fetch_ratings_template();
+
+		if ( $echo ) {
+			echo $ratings;
 		}
+
+		return $ratings;
 	}
 	// add action for template calls
-	add_action( 'wds_post_ratings', 'wds_post_ratings', 1, 1 );
+	add_action( 'wds_post_ratings', 'wds_post_ratings' );
 }
